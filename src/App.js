695a8d8f86b6850f38401ad8348e003b4d262ac3
SaiRakindi <sai.rakindi3@gmail.com>
2024-09-04 22:25:44 +0530

    feat: axios installation and useReducer product initialization

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useReducer } from "react";
+import axios from "axios";
 
 function App() {
+  const [state, dispatch] = useReducer(cartReducer, { products: [], cart: [] });
+
+  const fetchProducts = async () => {
+    const { data } = await axios.get("https://dummyjson.com/products");
+
+    dispatch({
+      type: "ADD_PRODUCTS",
+      payload: data.products,
+    });
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return <div>Hello</div>;
 }
 
